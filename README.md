dffdece71dcc3797c1d01c0345d5f15c8119c497
Brian R. Bondy <netzen@gmail.com>
2013-10-08 23:08:42 -0400

    Issue #2 - Setup redis dependencies and get basic loading / saving of db model entity for video

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ codefirefox
 ===========
 
 Tutorial site for coding Firefox and other Mozilla related technology
+
+Setting up the project on Windows
+=================================
+
+1. Extract `third_party/redisbin.zip` to a folder on your Desktop
+2. Copy `third_party/redis-win/redis.conf` to that same folder on your Desktop
+3. Run redis-server passing in the command line: redis.conf
+4. Install node.js (Current Version: v0.10.20)
+5. Run node index
+6. Browser to localhost:8088
+
+Versions:
+=========
+
+Install node.js (Current Version: v0.10.20)
+redis-server: https://github.com/MSOpenTech/redis/ (Windows based on Redis 2.6. The latest version merged in 2.6.12)
--- index.js
+++ index.js
@@ -1,6 +1,55 @@
 var express = require('express');
 var app = express();
 var dump = console.log;
+var redis = require("redis");
+var client = redis.createClient();
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
+
+function Video(slug, url, title, description) {
+  this.slug = slug;
+  this.url = url;
+  this.title = title;
+  this.description = description;
+}
+
+Videos = {
+
+  get: function(slug, callback, errCallback) {
+    if (!slug)
+      err();
+
+    client.get(slug, function(err, reply) {
+      if (!reply) {
+        errCallback();
+        return;
+      }
+
+      dump('Got: ' + reply.toString());
+
+      var obj = JSON.parse(reply.toString());
+      dump('Got2: ' + obj);
+      this.url = obj.url;
+      dump('Got3: ' + this.url);
+      this.title = obj.title;
+      this.description = obj.description;
+      callback(new Video(slug, obj.url, obj.title, obj.description));
+    });
+  },
+
+  set: function(video) {
+    if (!video.slug)
+      throw "slug must be set";
+    client.set(video.slug, JSON.stringify(video), redis.print);
+  },
+
+  initSampleData: function() {
+    Videos.set(new Video('brianbondy', 'http://www.brianbondy.com/', 'brian title here', 'brian description here'));
+    Videos.set(new Video('kamiljozwiak', 'http://kamiljozwiak.com/', 'kamil title', 'kamil description here'));
+    Videos.set(new Video('slashdot', 'http://www.slashdot.org/', 'slashdot title', 'slashdot description here'));
+  }
+}
 
 app.set('view engine', 'jade');
 app.set('view options', { layout: false, pretty: true });
@@ -9,14 +58,27 @@ app.use(express.bodyParser());
 app.use(express.limit('1mb'));
 
 app.get('/', function(req, res) {
-  res.render('index', { pageTitle: 'Code Firefox Videos', id: 0, bodyID: 'body_index'});
-});
-app.get('/:id(\\d+)*', function(req, res, next) {
-  if (req.params.id == 0) {
-    next(new Error('Could not find ID ' + req.params.id));
-    return;
-  }
-  res.render('index', { pageTitle: 'Code Firefox Video', id: req.params.id, bodyID: 'body_index'});
+  /*
+  var video = new Video('slug', 'http://www.brianbondy.com/', 'title here', 'description here');
+  Videos.set(video);
+  */
+  Videos.get('slug', function(video) {
+    res.render('index', { pageTitle: 'Code Firefox Videos', id: video.url, bodyID: 'body_index'});
+  });
 });
 
+app.get('/:slug', function(req, res, next) {
+  if (req.params.slug == 0) {
+    next(new Error('Could not find ID ' + req.params.slug));
+    return;
+  }
+
+  Videos.get(req.params.slug, function(video) {
+    res.render('index', { pageTitle: 'Code Firefox Videos', id: video.url, bodyID: 'body_index'});
+  }, function(err) {
+    res.render('index', { pageTitle: 'Code Firefox Videos', id: "Not Found", bodyID: 'body_index'});
+  });
+});
+
+Videos.initSampleData();
 app.listen(8088);
