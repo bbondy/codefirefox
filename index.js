b7fdfc76cfffc844ff0588f06d1705871caad89a
Brian R. Bondy <netzen@gmail.com>
2013-10-14 22:51:55 -0400

    Issue #10 - Add categories to video hierarchy

--- index.js
+++ index.js
@@ -10,15 +10,7 @@ client.on("error", function (err) {
   console.log("Error " + err);
 });
 
-function Video(slug, youtubeid, title, description, priority) {
-  this.slug = slug;
-  this.youtubeid= youtubeid;
-  this.title = title;
-  this.description = description;
-  this.priority = priority;
-}
-
-Videos = {
+DB = {
 
   get: function(slug, callback) {
     if (!slug)
@@ -30,50 +22,47 @@ Videos = {
       }
 
       var obj = JSON.parse(reply.toString());
-      this.youtubeid = obj.youtubeid;
-      this.title = obj.title;
-      this.description = obj.description;
-      this.priority = obj.priority;
-      callback(null, new Video(slug, obj.youtubeid, obj.title, obj.description, obj.priority));
+      callback(null, obj);
     });
   },
 
-  getAll: function(callback, errCallback) {
-    client.keys("*", function(err, replies) {
+  getAll: function(parentKey, callback, errCallback) {
+    client.keys((parentKey ? parentKey + ":" : "") + "*", function(err, replies) {
       if (!replies) {
         errCallback();
         return;
       }
 
       async.map(replies, function(key, mapCallback) {
-          Videos.get(key.toString(), mapCallback);
+          DB.get(key.toString(), mapCallback);
       }, callback);
 
     });
   },
 
-  set: function(video) {
-    if (!video.slug)
-      throw "slug must be set";
-    client.set(video.slug, JSON.stringify(video), redis.print);
+  set: function(key, obj) {
+    client.set(key, JSON.stringify(obj), redis.print);
   },
 
-  initData: function() {
-    fs.readFile('data/videos.json', 'utf8', function (err,data) {
+  initData: function(filePath) {
+    fs.readFile(filePath, 'utf8', function (err,data) {
       if (err) {
         return console.log(err);
       }
 
-      var arr = JSON.parse(data);
-      if (!arr) {
+      var categories = JSON.parse(data);
+      if (!categories) {
         return console.log(err);
       }
 
-      arr.forEach(function(video) {
-        Videos.set(video);
+      categories.forEach(function(category) {
+        DB.set("category:" + category.slug, category);
+        dump(JSON.stringify(category));
+        category.videos.forEach(function(video) {
+          DB.set(category.slug + ":" + video.slug, video);
+        });
       });
     });
-
   },
 
   sortByPriority: function(a, b) {
@@ -89,36 +78,31 @@ app.use(express.limit('1mb'));
 
 // URL: codefirefox.com/initData
 app.get('/initData', function(req, res) {
-  Videos.initData();
+  DB.initData('data/videos.json');
   res.render('simpleStatus', { pageTitle: 'Sample data initialized', status: "Sample Data initialized successfully", bodyID: 'body_index'});
 });
 
 // URL: codefirefox.com/
 app.get('/', function(req, res) {
-  Videos.getAll(function(err, videos) {
+  DB.getAll("category", function(err, categories) {
     if (err) {
       res.render('notFound', { pageTitle: 'Code Firefox Videos', id: "Couldn't find video", bodyID: 'body_not_found'});
       return;
     }
 
-    videos.sort(Videos.sortByPriority);
-
-    var videoURLs = videos.map(function(video) {
-      return video.url;
-    });
-
-    res.render('index', { pageTitle: 'Code Firefox Videos', videos: videos, bodyID: 'body_index'});
+    categories.sort(DB.sortByPriority);
+    res.render('index', { pageTitle: 'Code Firefox Videos', categories: categories, bodyID: 'body_index'});
   });
 });
 
-// URL: codefirefox.com/video1
-app.get('/:slug', function(req, res, next) {
-  if (req.params.slug == 0) {
-    next(new Error('Could not find ID ' + req.params.slug));
+// URL: codefirefox.com/category1/video1
+app.get('/:category/:video', function(req, res, next) {
+  if (req.params.category == 0 || req.params.video == 0) {
+    next(new Error('Invalid URL format, should be: category/video'));
     return;
   }
 
-  Videos.get(req.params.slug, function(err, video) {
+  DB.get(req.params.category + ":" + req.params.video, function(err, video) {
     if (err) {
       res.render('notFound', { pageTitle: 'Code Firefox Videos', id: "Couldn't find video", bodyID: 'body_not_found'});
       return;
