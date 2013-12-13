393b1383467ebb0b9d6ef387f1ff8b53757bb1c0
Brian R. Bondy <netzen@gmail.com>
2013-12-13 22:07:10 +0000

    Issue #150 - Add an entry for type in each videos entry (which will be renamed), add tests for valid fields in videos.json

--- test/test.js
+++ test/test.js
@@ -23,13 +23,29 @@ describe('files', function() {
     });
   }),
   describe('#videos.json()', function() {
-    it('should have a videos.json file without duplicate slugs', function(done) {
+    it('videos.json file should be valid', function(done) {
+       // Helps track duplicate entries
        var existsHelper = { };
        readFile(__dirname + '/../data/videos.json', 'utf8')
          .then(function(data) {
            var categories = JSON.parse(data);
 	   categories.forEach(function(c) {
+	       // Check that the category items are specified
+               assert.ok(c.title);
+               assert.ok(c.slug);
+               assert.ok(c.priority);
+               assert.ok(c.videos);
+
              c.videos.forEach(function(v) {
+
+               // Check that the video items are specified
+               assert(v.title);
+               assert(v.type);
+               assert(v.description !== undefined);
+               assert(v.youtubeid !== undefined);
+               assert(v.priority);
+               
+	       // Check for duplicate entry
                assert.ok(!existsHelper[v.slug], v.slug);
                existsHelper[v.slug] = true;
              });
