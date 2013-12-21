c4c4daef19bea42ab93e2d9aee3d41fa6525e256
Brian R. Bondy <netzen@gmail.com>
2013-12-20 23:08:32 -0500

    Issue #168, #156 - Use jquery ajax requests to wrap xhr everywhre. Delete stats now auto does a logout too.

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -4,82 +4,81 @@ $(document.links).filter(function() {
 
 /* Persona/Browser ID stuff */
 
-$("#login").click(function() {
+$('#login').click(function() {
   navigator.id.request();
 });
 
-$("#logout").click(function() {
-  console.log("logging out!");
-  console.log("Issuing logout");
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "/persona/logout", true);
-  xhr.addEventListener("loadend", function(e) {
-    console.log("reporting logout for user: " + email);
-    if (location.pathname != '/stats' ||
-        location.pathname != '/admin') {
-      location.reload(); 
-    } else {
-      location.href = '/';
+$('#logout').click(function() {
+  console.log('logging out!');
+  $.ajax({
+    url: '/persona/logout',
+    type: 'POST',
+    success: function(response) {
+      console.log('reporting logout for user: ' + email);
+      if (location.pathname.substring(0, 6) != '/stats' && 
+          location.pathname.substring(0, 6) != '/admin') {
+        console.log('reloading because pathname is: ' + location.pathname.substring(0, 6));
+        location.reload(); 
+      } else {
+        console.log('redirecting to /');
+        location.href = '/';
+      }
     }
   });
-  xhr.send();
-
   navigator.id.logout();
 });
 
-$("#del-stats").click(function() {
-  if (!confirm("Are you sure you want to completely nuke all of your user data?")) {
+$('#del-stats').click(function() {
+  if (!confirm('Are you sure you want to completely nuke all of your user data?')) {
     return;
   }
 
-  var xhr = new XMLHttpRequest();
-  xhr.open("DELETE", "/stats", true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.addEventListener("loadend", function(e) {
-    var data = JSON.parse(this.responseText);
-    if (data && data.status === "okay") {
-      console.log("Data was successfully deleted");
-      location.reload(); 
+  $.ajax({
+    url: '/stats',
+    type: 'DELETE',
+    contentType: 'application/json',
+    dataType: 'json',
+    success: function(response) {
+      if (response.status === 'okay') {
+        console.log('Data was successfully deleted');
+        $('#logout').click();
+      }
     }
-  }, false);
-
-  xhr.send(JSON.stringify({
-  }));
+  });
 });
 
 $(function() {
   // By default, express-persona adds the users email address to req.session.email when their email is validated.
   navigator.id.watch({
     onlogin: function(assertion) {
-      console.log("onlogin");
+      console.log('onlogin');
       // Check for already logged in
       if (email) {
         return;
       }
-
-      var xhr = new XMLHttpRequest();
-      xhr.open("POST", "/persona/verify", true);
-      xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.addEventListener("loadend", function(e) {
-        var data = JSON.parse(this.responseText);
-        if (data && data.status === "okay") {
-          console.log("reporting login for user: " + email);
-          location.reload(); 
+      $.ajax({
+        url: '/persona/verify',
+        type: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({assertion: assertion}),
+        success: function(response) {
+          if (response.status === 'okay') {
+            console.log('reporting login for user: ' + email);
+            location.reload(); 
+          }
         }
-      }, false);
-
-      xhr.send(JSON.stringify({
-        assertion: assertion
-      }));
+      });
     },
+
     onlogout: function() {
       // For some reason we're getting onlogout calls when we shouldn't.
       // To avoid this I put the actual xhr calls inside the logout button
       // handler.
       if (!email) {
-        console.log("persona calling onlogout with known email");
+        console.log('persona calling onlogout with known email');
       } else {
-        console.log("persona calling onlogout with unknown email");
+        console.log('persona calling onlogout with unknown email');
       }
     }
   });
