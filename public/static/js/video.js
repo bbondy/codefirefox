84c5262ed8145e82b6f6b6b2ee158130d6396b8b
Brian R. Bondy <netzen@gmail.com>
2013-12-23 19:18:13 -0500

    Issue #76 - Add code to support amara captioning

--- public/static/js/video.js
+++ public/static/js/video.js
@@ -1,33 +1,14 @@
-// Load the iframe player API async
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// Create a youtube iframe after the API code downloads
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '506',
-    width: '900',
-    videoId: youtubeid,
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
-}
-
-// Video player is ready
-function onPlayerReady(event) {
+function onYouTubePlayerReady(id) {
+  var player = document.getElementsByTagName('object')[0];
   player.setPlaybackQuality('hd1080');
-  event.target.playVideo();
+  player.playVideo();
+  player.addEventListener("onStateChange", "onPlayerStateChange");
 }
 
 // Video in player ends
-function onPlayerStateChange(event) {
+function onPlayerStateChange(data) {
   // Later I'll add code to callback to report video as watched here
-  if (event.data === 0) {
+  if (data === 0) {
     console.log("Reporting video watched");
     var xhr = new XMLHttpRequest();
     xhr.open("POST", '/video/' + videoSlug, true);
