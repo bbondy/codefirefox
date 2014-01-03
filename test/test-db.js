fc46d56f990db8469bbb73eb140131024a5accdb
Brian R. Bondy <netzen@gmail.com>
2014-01-03 00:27:06 -0500

    Issue #190 - unit tests for db.getSetElements and db.addToSet

--- test/test-db.js
+++ test/test-db.js
@@ -20,7 +20,7 @@ describe('db module', function() {
        assert(false);
      });
   }), 
-  it('getAll wiht 2 items should get both items', function(done) {
+  it('getAll with 2 items should get both items', function(done) {
      var key1 = 'test:2:1',
        val1 = { p1: '3.14', p2: [1] },
        key2 = 'test:2:2',
@@ -30,6 +30,27 @@ describe('db module', function() {
        return db.setOnePromise(key2, val2);
      }).then(function() {
        return db.getAllPromise('test:2');
+     }).done(function onSuccess(data) {
+       assert(data.length == 2);
+       assert(data[0].p1 == val1.p1);
+       assert(data[0].p2[0] == val1.p2[0]);
+       assert(data[1].p1 == val2.p1);
+       assert(data[1].p2[0] == val2.p2[0]);
+       done();
+     }, function onFailure(err) {
+       console.log(err);
+       assert(false);
+     });
+  }),
+  it('db.getSetElements and db.addToSet x 2 should work', function(done) {
+     var key = 'test:3',
+       val1 = { p1: '3.14', p2: [1] },
+       val2 = { p1: '159', p2: [42] };
+
+     db.addToSetPromise(key, val1).then(function() {
+       return db.addToSetPromise(key, val2);
+     }).then(function() {
+       return db.getSetElementsPromise(key);
      }).done(function onSuccess(data) {
        assert(data.length == 2);
        assert(data[0].p1 == val1.p1);
