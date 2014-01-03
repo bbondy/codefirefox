700af6909daaac2b9f988351a85563987b9eb7e3
Brian R. Bondy <netzen@gmail.com>
2014-01-03 00:15:46 -0500

    Issue #190 - Add unit test for db.getAll and db.set x 2

--- test/test-db.js
+++ test/test-db.js
@@ -19,5 +19,27 @@ describe('db module', function() {
        console.log(err);
        assert(false);
      });
+  }), 
+  it('getAll wiht 2 items should get both items', function(done) {
+     var key1 = 'test:2:1',
+       val1 = { p1: '3.14', p2: [1] },
+       key2 = 'test:2:2',
+       val2 = { p1: '159', p2: [42] };
+
+     db.setOnePromise(key1, val1).then(function() {
+       return db.setOnePromise(key2, val2);
+     }).then(function() {
+       return db.getAllPromise('test:2');
+     }).done(function onSuccess(data) {
+       assert(data.length == 2);
+       assert(data[0].p1 == val1.p1);
+       assert(data[0].p2[0] == val1.p2[0]);
+       assert(data[1].p1 == val2.p1);
+       assert(data[1].p2[0] == val2.p2[0]);
+       done();
+     }, function onFailure(err) {
+       console.log(err);
+       assert(false);
+     });
   });
 });
