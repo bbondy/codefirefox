2fde373398b1f33d0c197c2414a089e6a8f13aeb
Brian R. Bondy <netzen@gmail.com>
2014-01-06 22:41:41 -0500

    Issue #51 - Add an rss feed for new videos and exercises

---
+++ test/test-rssController.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var assert = require("assert"),
+  Promise = require("promise"),
+  _ = require("underscore"),
+  lessonController = require('../controllers/lessonController.js'),
+  rssController = require('../controllers/rssController.js');
+
+describe('rssController', function() {
+  it('init should make the items property available', function(done) {
+    assert(!rssController.items);
+    lessonController.init(function(err) {
+      rssController.init(lessonController.categories, function(err) {
+        assert(!err);
+        assert(_.isArray(rssController.items));
+        assert(rssController.items.length);
+        assert(_.isString(rssController.getFeedXML('http://codefirefox.com')));
+        done();
+      });
+    });
+  });
+});
+
