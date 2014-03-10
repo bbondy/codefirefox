e4ded91806e1b9098fcf16a0aa36982cb4559ae9
Brian R. Bondy <netzen@gmail.com>
2014-03-10 10:30:08 -0400

    Issue #230 - Add Mozilla GA code

--- public/static/js/analytics.js
+++ public/static/js/analytics.js
@@ -1,3 +1,15 @@
+// See Bug 977131 - Mozilla provided GA code
+// https://bugzilla.mozilla.org/show_bug.cgi?id=977131
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-35433268-57', 'codefirefox.com');
+  ga('send', 'pageview');
+
+/*
+Old code:
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -5,3 +17,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-45042006-1', 'codefirefox.com');
 ga('send', 'pageview');
+*/
