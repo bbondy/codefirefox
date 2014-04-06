055caea45701ee885cb24f15d12baee935bd4b05
Brian R. Bondy <netzen@gmail.com>
2014-04-05 23:37:09 -0400

    Revert back to codefirefox.com personal account GA

--- public/static/js/analytics.js
+++ public/static/js/analytics.js
@@ -1,3 +1,4 @@
+/*
 // See Bug 977131 - Mozilla provided GA code
 // https://bugzilla.mozilla.org/show_bug.cgi?id=977131
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -7,8 +8,8 @@
 
   ga('create', 'UA-35433268-57', 'codefirefox.com');
   ga('send', 'pageview');
+*/
 
-/*
 Old code:
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -17,4 +18,3 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-45042006-1', 'codefirefox.com');
 ga('send', 'pageview');
-*/
